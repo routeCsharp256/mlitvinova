37f01a1fc40d223e823de8e3e2d72c845c900eb5
Maria <mariia.v.litvinova@gmail.com>
2021-10-26 00:05:55 +0300

    draft service

---
+++ src/OzonEdu.MerchandiseService/GrpcServices/MerchApiGrpService.cs
@@ -0,0 +1,17 @@
+﻿using MerchandiseService.Services.Interfaces;
+using OzonEdu.StockApi.Grpc;
+
+namespace MerchandiseService.GrpcServices
+{
+    public class MerchApiGrpService : MerchandiseApiGrpc.MerchandiseApiGrpcBase
+    {
+        private readonly IMerchandiseService _service;
+
+        public MerchApiGrpService(IMerchandiseService service)
+        {
+            _service = service;
+        }
+        
+        
+    }
+}
