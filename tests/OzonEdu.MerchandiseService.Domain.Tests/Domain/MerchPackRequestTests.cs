71d57b4fd55d139f20f622eed176102fd4403a91
Maria <mariia.v.litvinova@gmail.com>
2021-11-11 09:31:37 +0300

    add tests for constraints

--- tests/OzonEdu.MerchandiseService.Domain.Tests/Domain/MerchPackRequestTests.cs
+++ tests/OzonEdu.MerchandiseService.Domain.Tests/Domain/MerchPackRequestTests.cs
@@ -1,7 +1,43 @@
-﻿namespace OzonEdu.MerchandiseService.Domain.Tests.Domain
+﻿using System.Collections.Generic;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.EmployeeAggregate;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchPackAggregate;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchPackRequestAggregate;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.StockItemAggregate;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.ValueObjects;
+using Xunit;
+
+namespace OzonEdu.MerchandiseService.Domain.Tests.Domain
 {
     public class MerchPackRequestTests
     {
-        
+        [Fact]
+        public void ValidateConstraints_ConstraintsAreResolvable_NoClothes_ShouldExecuteSuccessfully()
+        {
+            var stockItems = new List<StockItem>()
+            {
+                new(new Sku(1), new Name("Test bag"), new Item(ItemType.Bag), null, new Quantity(10))
+            };
+            
+            var merchPack = new MerchPack(new MerchPackName("Test"), new List<MerchItem>()
+            {
+                new(ItemType.Bag, new List<GenericMerchConstraint>()
+                {
+                    new GenericMerchConstraint("Print", "Test")
+                })
+            });
+            var employee = new Employee(1);
+            var constraints = new List<IMerchConstraint>()
+            {
+                new GenericMerchConstraint("Color", "Red")
+            };
+
+            var merchRequest = new MerchPackRequest(
+                employee,
+                merchPack,
+                constraints);
+
+            var filteredSku = merchRequest.FilterByConstraints(stockItems);
+            Assert.Single(filteredSku);
+        }
     }
 }
