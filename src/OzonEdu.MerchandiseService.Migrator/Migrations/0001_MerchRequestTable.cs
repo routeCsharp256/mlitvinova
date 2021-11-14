0ccc9ba828e0fb5e4108a132a1c7d3d97fa81d6c
Maria <mariia.v.litvinova@gmail.com>
2021-11-15 01:30:04 +0300

    add migrations project

---
+++ src/OzonEdu.MerchandiseService.Migrator/Migrations/0001_MerchRequestTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace OzonEdu.MerchandiseService.Migrator.Migrations
+{
+    [Migration(1)]
+    public class MerchRequestTable : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"
+                    CREATE TABLE if not exists MerchRequests(
+                        id BIGSERIAL PRIMARY KEY,
+                        name TEXT NOT NULL,
+                        item_type_id INT NOT NULL,
+                        clothing_size INT);"
+            );
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DROP TABLE if exists MerchRequests;");
+        }
+    }
+}
